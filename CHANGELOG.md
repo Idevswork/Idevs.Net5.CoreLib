486dd07793f95db734c4c8bc568ec11286cf1ca5
Sarawut Phaekuntod <sarawut@idevs.work>
2024-08-17 10:39:58 +0700

    ChangeLog 0.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 0.0.4 (2024-08-17)
+
+### Update
+
+- Add browserPath to PdfExporter.Export to use existing Chrome installed instead of download everytime.
+
 ## 0.0.3 (2023-10-08)
 
 ### Add
--- Services/PdfExporter.cs
+++ Services/PdfExporter.cs
@@ -5,7 +5,7 @@ namespace Idevs.Services
 {
     public interface IIdevsPdfExporter
     {
-        byte[] Export(string html, string headerTemplate = "<p></p>", string footerTemplate = "<p></p>");
+        byte[] Export(string html, string headerTemplate = "<p></p>", string footerTemplate = "<p></p>", string browserPath = "");
     }
 
     public class IdevsPdfExporter : IIdevsPdfExporter
@@ -13,20 +13,35 @@ namespace Idevs.Services
         public byte[] Export(
             string html,
             string headerTemplate = "<p></p>",
-            string footerTemplate = "<p></p>")
+            string footerTemplate = "<p></p>",
+            string browserPath = "")
         {
-            var pdfBytes = Task.Run(async () => await DoGeneratePdf(html, headerTemplate, footerTemplate)).Result;
+            var pdfBytes = Task.Run(async () => await DoGeneratePdf(html, headerTemplate, footerTemplate, browserPath)).Result;
             return pdfBytes;
         }
 
         private static async Task<byte[]> DoGeneratePdf(
             string html,
             string headerTemplate = "<p></p>",
-            string footerTemplate = "<p></p>")
+            string footerTemplate = "<p></p>",
+            string browserPath = "")
         {
-            using var browserFetcher = new BrowserFetcher();
-            await browserFetcher.DownloadAsync();
-            await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+            var launchOption = new LaunchOptions
+            {
+                Headless = true
+            };
+
+            if (string.IsNullOrEmpty(browserPath))
+            {
+                using var browserFetcher = new BrowserFetcher();
+                await browserFetcher.DownloadAsync();
+            }
+            else
+            {
+                launchOption.ExecutablePath = browserPath;
+            }
+
+            await using var browser = await Puppeteer.LaunchAsync(launchOption);
             await using var page = await browser.NewPageAsync();
             await page.SetContentAsync(html, new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation. Networkidle0 }});
             return await page.PdfDataAsync(new PdfOptions
